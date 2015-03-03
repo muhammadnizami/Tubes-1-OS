612fb3ecbab04c83fa271f466dcd3a312af0e997
Muhammad Nizami <nizami_muhammad@yahoo.co.id>
2015-03-03 11:16:47 +0700

    ada eksekusi

--- src/eksekusi.cpp
+++ src/eksekusi.cpp
@@ -1,6 +1,8 @@
 #include <assert.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 bool isExecutable(const char * path){
 //mengembalikan true bila path menunjuk berkas eksekutabel
@@ -11,11 +13,17 @@ void eksekusi(const char * path, char * argv[]){
 //I.S. isExecutable(path)
 //F.S. terbentuk proses anak yang menjalankan path
 	pid_t pid;
+	int status;
 	pid=fork();
 	if (pid==0){	
 		execvp(path,argv);
 	}else {
 		assert(pid>0);
-		wait(pid);
+		waitpid(pid, &status, WUNTRACED
+#ifdef WCONTINUED       /* Not all implementations support this */
+        | WCONTINUED
+#endif
+		);
+		fflush(stdout);
 	}
 }
--- src/main.cpp
+++ src/main.cpp
@@ -33,19 +33,14 @@ int main(){
 		printf("\n");
 		
 		if (argc>0){
-			if (!strcmp(argv[0],"cd" && argv[1]!=NULL)){
+			if (!strcmp(argv[0],"cd") && argv[1]!=NULL){
 				if (cd(argv[1])==-1)
 					printf("cd: %s: No such file or directory\n",argv[1]);
-			}
+			}else	eksekusi(argv[0],argv);
 		
-
-	//		if (isExecutable(path))
-	//			eksekusi(path,argv);
-	//		else
-	//			printf("%s: command not found\n",argv[0]);
-
-			//cleanup argv
+			
 		}
+		//cleanup argv
 		for (int j=0;j<argc;j++) delete argv[j];
 	}
 }
